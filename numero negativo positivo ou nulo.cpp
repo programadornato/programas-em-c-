f33827551f5d2b18c38b4801b1eb84a41af9c3c4
yuri cavalcante <102636657+programadornato@users.noreply.github.com>
2023-06-03 21:23:26 -0300

    Create numero negativo positivo ou nulo.cpp
    Motivação
    Chaves, se eu tenho 5 bananas e como 5 bananas, com o que eu fico?
    Prisão de ventre!
    Não chaves, suponha que eu tenha 5 mamões e como os 5, com o que eu fico?
    Intestino frouxo!
    Chaves, não seja burro! Se você TINHA 5 moedas, cada sanduíche era uma moeda e você comeu 5 sanduíches, com quanto você fica?
    1 moeda!
    Mas como uma moeda Chaves, você fica com zero, 0, ZERO!!!!
    Não, é porque na mercearia do Chiquim a promoção é leve 5 e pague 4.
    O Chaves não sabe diferenciar positivo, negativo e nulo, mas você sabe! Eu espero!

---
+++ numero negativo positivo ou nulo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int number;
+cin >> number;
+
+if(number > 0) cout << "positivo";
+
+else if(number < 0) cout << "negativo";
+
+else if(number == 0) cout << "nulo";
+}
